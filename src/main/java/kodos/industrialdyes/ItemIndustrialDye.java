96a7e4cff343ca530b109b9951cf2b285e75fe7e
Kodos Macarthur <MyNameIsKodos@users.noreply.github.com>
2015-11-21 07:34:46 -0600

    Update ItemIndustrialDye.java
    You see nothing.

--- src/main/java/kodos/industrialdyes/ItemIndustrialDye.java
+++ src/main/java/kodos/industrialdyes/ItemIndustrialDye.java
@@ -3,7 +3,7 @@ package kodos.industrialdyes;
 import net.minecraft.item.ItemDye;
 
 /**
- * Created by Joshua on 11/21/2015.
+ * Created by Kodos on 11/21/2015.
  */
 public class ItemIndustrialDye extends ItemDye {
 
