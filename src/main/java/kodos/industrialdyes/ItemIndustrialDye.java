839dbb7ed2940a8ad799aebfc65cd21c1b6af5d0
Kodos Macarthur <MyNameIsKodos@users.noreply.github.com>
2015-11-21 08:10:48 -0600

    Update ItemIndustrialDye.java

--- src/main/java/kodos/industrialdyes/ItemIndustrialDye.java
+++ src/main/java/kodos/industrialdyes/ItemIndustrialDye.java
@@ -7,7 +7,7 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 /**
- * Created by Kodos on 11/21/2015.
+ * Created by Joshua on 11/21/2015.
  */
 public class ItemIndustrialDye extends ItemDye {
     @SideOnly(Side.CLIENT)
@@ -19,10 +19,16 @@ public class ItemIndustrialDye extends ItemDye {
 
     @SideOnly(Side.CLIENT)
     public void registerIcons(IIconRegister iconregister) {
-        icons = new IICon[16];
+        icons = new IIcon[16];
 
-        for(int i = 0; i < icons.length;, i++) {
+        for(int i = 0; i < icons.length; i++) {
             icons[i] = iconregister.registerIcon(IndustrialDyes.MODID + ":" + (this.getUnlocalizedName().substring(5)) + i);
         }
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public IIcon getIcon(Itemstack item, int pass) {
+        
     }
 }
