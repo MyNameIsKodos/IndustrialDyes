622168cdb4ad339f3425f8b6e6ec6b5a4d261141
Kodos Macarthur <MyNameIsKodos@users.noreply.github.com>
2015-11-21 08:25:33 -0600

    Update ItemIndustrialDye.java

--- src/main/java/kodos/industrialdyes/ItemIndustrialDye.java
+++ src/main/java/kodos/industrialdyes/ItemIndustrialDye.java
@@ -1,14 +1,16 @@
 package kodos.industrialdyes;
 
+import java.util.List;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemDye;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-/**
- * Created by Joshua on 11/21/2015.
- */
+
 public class ItemIndustrialDye extends ItemDye {
     @SideOnly(Side.CLIENT)
     private IIcon[] icons;
@@ -28,7 +30,15 @@ public class ItemIndustrialDye extends ItemDye {
 
     @SideOnly(Side.CLIENT)
     @Override
-    public IIcon getIcon(Itemstack item, int pass) {
-        
+    public IIcon getIconFromDamage(int meta){
+        if (meta > 16) { meta = 0; }
+        return this.icons(meta);
+    }
+
+    @Override
+    public void getSubItems(Item item, CreativeTabs tab, List list) {
+        for(int i = 0, i < icons.length; i++) {
+            list.add(new ItemStack(item, l, i));
+        }
     }
 }
