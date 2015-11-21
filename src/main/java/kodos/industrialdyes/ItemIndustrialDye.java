5aec411a3d593c7a658fea226d0c17a1d5c3d7e6
Kodos Macarthur <MyNameIsKodos@users.noreply.github.com>
2015-11-21 07:39:36 -0600

    Update ItemIndustrialDye.java

--- src/main/java/kodos/industrialdyes/ItemIndustrialDye.java
+++ src/main/java/kodos/industrialdyes/ItemIndustrialDye.java
@@ -1,10 +1,18 @@
 package kodos.industrialdyes;
 
 import net.minecraft.item.ItemDye;
+import net.minecraft.util.IIcon;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 /**
  * Created by Kodos on 11/21/2015.
  */
 public class ItemIndustrialDye extends ItemDye {
+    @SideOnly(Side.CLIENT)
+    private IIcon[] icons;
 
+    public ItemIndustrialDye(IIcon[] icons) {
+        this.icons = icons;
+    }
 }
