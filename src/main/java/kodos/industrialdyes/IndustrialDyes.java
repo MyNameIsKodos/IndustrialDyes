4fcc14da69c5e9cc44bc1b3dcedcd7e5e01aea96
Kodos Atoz <jge84@yahoo.com>
2015-11-23 17:00:52 -0600

    Updating remote with current mod build

--- src/main/java/kodos/industrialdyes/IndustrialDyes.java
+++ src/main/java/kodos/industrialdyes/IndustrialDyes.java
@@ -3,22 +3,41 @@ package kodos.industrialdyes;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 
-@Mod(modid = kodos.industrialdyes.IndustrialDyes.MODID, version = kodos.industrialdyes.IndustrialDyes.VERSION)
+@Mod(modid = IndustrialDyes.MODID, version = IndustrialDyes.VERSION)
 
 public class IndustrialDyes
 {
     public static final String MODID = "industrialdyes";
     public static final String VERSION = "0.1";
+    public static Item itemIndustrialDye;
+
+    public static CreativeTabs tabIDyes = new CreativeTabs("industrialDyes") {
+        @Override
+        public Item getTabIconItem() {
+            return new ItemStack(itemIndustrialDye, 1, 15).getItem(); //TODO Make the stupid icon use the white dye.
+        }
+    };
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
 		// some example code
         System.out.println(MODID + ": This is a test.");
+
+        itemIndustrialDye = new ItemIndustrialDye();
+
+        GameRegistry.registerItem(itemIndustrialDye, "industrialDye");
+
+        GameRegistry.addRecipe(new ShapedOreRecipe();
+
         OreDictionary.registerOre("dye", new ItemStack(itemIndustrialDye, 1, 32767));
         OreDictionary.registerOre("dyeBlack", new ItemStack(itemIndustrialDye, 1, 0));
         OreDictionary.registerOre("dyeRed", new ItemStack(itemIndustrialDye, 1, 1));
--- src/main/java/kodos/industrialdyes/ItemIndustrialDye.java
+++ src/main/java/kodos/industrialdyes/ItemIndustrialDye.java
@@ -1,10 +1,47 @@
 package kodos.industrialdyes;
 
+import java.util.List;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemDye;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
-/**
- * Created by Joshua on 11/21/2015.
- */
+
 public class ItemIndustrialDye extends ItemDye {
+    @SideOnly(Side.CLIENT)
+    private IIcon[] icons;
 
+    public ItemIndustrialDye() {
+        super();
+        setHasSubtypes(true);
+        this.setCreativeTab(IndustrialDyes.tabIDyes);
+        this.setUnlocalizedName("industrialDye");
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconregister) {
+        icons = new IIcon[16];
+
+        for(int i = 0; i < icons.length; i++) {
+            icons[i] = iconregister.registerIcon(IndustrialDyes.MODID + ":" + (this.getUnlocalizedName().substring(5)) + i);
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public IIcon getIconFromDamage(int meta){
+        if (meta > 16) { meta = 0; }
+        return this.icons[meta];
+    }
+
+    @Override
+    public void getSubItems(Item item, CreativeTabs tab, List list) {
+        for(int i = 0; i < 16; i++) {
+            list.add(new ItemStack(item, 1, i));
+        }
+    }
 }
